a631320f086a4b63858e091b50e0374143a0678a
Takordy <takordy@gmail.com>
2023-04-04 22:18:06 +0200

    Final bug fixes

--- script.js
+++ script.js
@@ -14,15 +14,29 @@ const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
 // Starting coditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
 
-let scores = [0, 0];
+let scores, currentScore, activePlayer, playing;
 
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+const init = () => {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  diceEl.classList.add('hidden');
+
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player1El.classList.remove('player--active');
+  player0El.classList.add('player--active');
+};
+
+init();
 
 const switchPlayer = () => {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -32,13 +46,6 @@ const switchPlayer = () => {
   player1El.classList.toggle('player--active');
 };
 
-const resetScore = score => {
-  score.textContent = 0;
-};
-
-const resetCurrentScore = score => {
-  score.textContent = 0;
-};
 // Rolling dice funtionality
 
 btnRoll.addEventListener('click', function () {
@@ -73,7 +80,7 @@ btnHold.addEventListener('click', function () {
     document.getElementById(`score--${activePlayer}`).textContent =
       scores[activePlayer];
     // 2. Check if player's score is >= 100
-    if (scores[activePlayer] >= 20) {
+    if (scores[activePlayer] >= 100) {
       // Finish the game
       playing = false;
       diceEl.classList.add('hidden');
@@ -93,14 +100,4 @@ btnHold.addEventListener('click', function () {
 
 // Reseting the game to default statement
 
-btnNew.addEventListener('click', function () {
-  resetScore(score0El);
-  resetScore(score1El);
-  resetCurrentScore(current0El);
-  resetCurrentScore(current1El);
-  playing = true;
-  scores = [0, 0];
-  document.querySelector('.player--0').classList.remove('player--winner');
-  document.querySelector('.player--0').classList.add('player--active');
-  document.querySelector('.player--1').classList.remove('player--winner');
-});
+btnNew.addEventListener('click', init);
