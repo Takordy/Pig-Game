2935a5eaa5cde96061ee31d7c76c8a027dda7e70
Takordy <takordy@gmail.com>
2023-04-04 21:45:55 +0200

    Adding reseting to default funtionality on New Game btn

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
-const scores = [0, 0];
+let scores = [0, 0];
 
 let currentScore = 0;
 let activePlayer = 0;
@@ -32,6 +32,13 @@ const switchPlayer = () => {
   player1El.classList.toggle('player--active');
 };
 
+const resetScore = score => {
+  score.textContent = 0;
+};
+
+const resetCurrentScore = score => {
+  score.textContent = 0;
+};
 // Rolling dice funtionality
 
 btnRoll.addEventListener('click', function () {
@@ -69,6 +76,8 @@ btnHold.addEventListener('click', function () {
     if (scores[activePlayer] >= 20) {
       // Finish the game
       playing = false;
+      diceEl.classList.add('hidden');
+      currentScore = 0;
       document
         .querySelector(`.player--${activePlayer}`)
         .classList.add('player--winner');
@@ -80,4 +89,18 @@ btnHold.addEventListener('click', function () {
       switchPlayer();
     }
   }
+});
+
+// Reseting the game to default statement
+
+btnNew.addEventListener('click', function () {
+  resetScore(score0El);
+  resetScore(score1El);
+  resetCurrentScore(current0El);
+  resetCurrentScore(current1El);
+  playing = true;
+  scores = [0, 0];
+  document.querySelector('.player--0').classList.remove('player--winner');
+  document.querySelector('.player--0').classList.add('player--active');
+  document.querySelector('.player--1').classList.remove('player--winner');
 });
